39bfa70a587ff09bcbf7f62d62a53833b5b7acd1
axlrose <3444542@163.com>
2010-05-31 14:12:43 +0800

    ???????????? autostat.sh???????

--- profiles/archlive/overlay/home/arch/.config/openbox/autostart.sh
+++ profiles/archlive/overlay/home/arch/.config/openbox/autostart.sh
@@ -8,7 +8,39 @@ if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
        eval `dbus-launch --sh-syntax --exit-with-session`
 fi
 
-USE_IBUS=1
+USE_IBUS=1   #use ibus input 
+USE_SCIM=0   #use scim input
+USE_FCITX=0   #use fcitx input
+
+#=======================================
+if [ ${USE_SCIM} -eq 1 ] ;then
+    export XIM=scim
+    export XMODIFIERS="@im=scim"
+    export XIM_PROGRAM="/usr/bin/scim"
+    export GTK_IM_MODULE="scim-bridge"
+    export QT_IM_MODULE="scim-bridge"
+    export XIM_ARGS="-d"
+
+    if [ -z `pidof scim` ] ; then
+        scim-birdge  &
+    fi
+fi
+
+#=======================================
+
+if [ ${USE_FCITX} -eq 1 ] ;then
+    export XIM=fcitx
+    export XMODIFIERS="@im=fcitx"
+    export XIM_PROGRAM="/usr/bin/fcitx"
+    export GTK_IM_MODULE="fcitx"
+    export QT_IM_MODULE="fcitx"
+    export XIM_ARGS="-d"
+
+    if [ -z `pidof fcitx` ] ; then
+        fcitx &
+    fi
+fi
+
 #=======================================
 if [ ${USE_IBUS} -eq 1 ] ;then
     export XMODIFIERS="@im=ibus"
@@ -23,6 +55,7 @@ if [ ${USE_IBUS} -eq 1 ] ;then
 fi
 #=======================================
 
+pcmanfm -d &
 # Make GTK apps look and behave how they were set up in the gnome config tools
 [ -f /usr/libexec/gnome-settings-daemon ] && eval `/usr/libexec/gnome-settings-daemon & >/dev/null 2>&1` 
 
@@ -42,8 +75,8 @@ export OOO_FORCE_DESKTOP=gnome
 
 feh --bg-scale ~/.config/wallpapers/Tiger_Blue.jpg
 
-killall xcompmgr >/dev/null
-xcompmgr -Ss -n -Cc -fF -I-10 -O-10 -D1 -t-3 -l-4 -r4 &
+#killall xcompmgr >/dev/null
+#xcompmgr -Ss -n -Cc -fF -I-10 -O-10 -D1 -t-3 -l-4 -r4 &
 
 # set panel
 #killall tint2 > /dev/null 2>&1
